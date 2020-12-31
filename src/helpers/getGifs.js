0b14fa09a05816514d8654ba8cbd185cf1a942bc
Sergio Salinas <sergio-salinas@outlook.com>
2020-12-31 10:35:07 +0100

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,19 @@
+
+
+export const getGifs = async (category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?limit=10&q=${encodeURI(category)}&api_key=toyuy2PIARomkxJP6iabN3Hgrlfg96Qo`;
+
+    const resp = await fetch ( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+}
